b03a8edbc96de3cf6c8f9f49308d635bb569217c
Paweł Grabacki <paw.grabacki@gmail.com>
2026-08-10 21:01:06 +0200

    Init tf+ passed validation

---
+++ src/main.tf
@@ -0,0 +1,25 @@
+terraform {
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+
+    }
+}
+
+provider "aws"{
+region = "eu-central-1"
+profile = "pawgr"
+}
+
+resource "aws_s3_bucket" "test"{
+    bucket_prefix = "testbucket-"
+    force_destroy = true
+}
+
+output "bucket_name"{
+    value = aws_s3_bucket.test
+}
+
