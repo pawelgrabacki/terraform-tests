0a0d16dd7ea5ed9c65b24f5cf3f65e4d4933a93f
Paweł Grabacki <paw.grabacki@gmail.com>
2026-08-22 20:28:06 +0200

    azure groups/storage account fixes

--- src/main.tf
+++ src/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "test" {
-  name                     = "storagetest4321234"
+  name                     = "stg-group"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
