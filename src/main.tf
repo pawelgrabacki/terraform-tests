c0c5400ae027301b2d2ee845fd0dfb0f4cb40e53
Paweł Grabacki <paw.grabacki@gmail.com>
2026-08-22 13:02:52 +0200

    tf validate fixes

--- src/main.tf
+++ src/main.tf
@@ -1,23 +1,10 @@
-terraform {
+#main.tf
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region  = "eu-central-1"
-  profile = "pgtf"
-}
-
+#aws
 resource "aws_s3_bucket" "test" {
   bucket_prefix = "testbucket-"
   force_destroy = true
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.test.bucket
-}
+
+#gcp
--- src/providers.tf
+++ src/providers.tf
@@ -1,4 +1,20 @@
 #providers.tf
+terraform {
+  required_version = ">=1.5"
+
+  required_providers {
+    
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+
+    google = {
+        source = "hashicorp/google"
+        version = "~>6.0"
+    }
+  }
+}
 #aws
 provider "aws"{
     profile = var.aws_profile
